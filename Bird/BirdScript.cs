43dbf3cf6bbf6099ea49370f7d37aa4fed641bc7
Tyler Yu <tyleryy@uci.edu>
2023-08-27 02:27:56 -0700

    completed game w/ floor and ceil

--- Bird/BirdScript.cs
+++ Bird/BirdScript.cs
@@ -7,19 +7,32 @@ public class BirdScript : MonoBehaviour
 
     public Rigidbody2D myRigidBody; // reference to components on gameObject
     public float flapStrength = 20; // variable
+    public LogicScript logic; // ties to logic manager
+    public bool birdIsAlive = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>(); // Searches for GameObject with tag "Logic", then gets its component called LogicScript
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidBody.velocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != 6)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
         }
     }
 }
--- Logic/LogicScript.cs
+++ Logic/LogicScript.cs
@@ -9,6 +9,7 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text scoreText; // reference to Text GameObject on UI
                            // here we can configure what variables and references we want to show in Unity editor for this game Object
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -26,5 +27,6 @@ public class LogicScript : MonoBehaviour
     public void gameOver()
     {
         gameOverScreen.SetActive(true);
+
     }
 }
