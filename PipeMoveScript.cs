410a58110e32f42818c967bf700b10b154878b3f
Tyler Yu <tyleryy@uci.edu>
2023-08-27 00:56:12 -0700

    deletes pipes

--- PipeMoveScript.cs
+++ PipeMoveScript.cs
@@ -6,6 +6,7 @@ public class PipeMoveScript : MonoBehaviour
 {
 
     public float moveSpeed = 5;
+    public float deadZone = -45;
 
 
     // Start is called before the first frame update
@@ -18,5 +19,11 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime; // Time.deltaTime ties the position change rate by an internal clock (not platform dependent)
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
+
     }
 }
